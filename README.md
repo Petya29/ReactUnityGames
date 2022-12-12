02de2e57c60f4ae4fe95b72bdeedf69bbb7803ee
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-12-12 17:00:52 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Site with games, global score anf achievements
+
+Made with React, Node.js, Express, Prisma, Typescript and Unity
---
+++ server/index.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res, next) => {
+    res.send('Hello from typescript!!!');
+});
+
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server start on ${PORT} port: http://localhost:${PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
