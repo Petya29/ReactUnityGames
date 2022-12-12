d593cc68c2c858198925af2b345d496604e29144
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-12-12 22:07:14 +0100

    Added global declarartions and .env example

---
+++ server/environment.d.ts
@@ -0,0 +1,18 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            PORT: string;
+            API_URL: string;
+            CLIENT_URL: StringLitera;
+            JWT_ACCESS_SECRET_KEY: string;
+            JWT_REFRESH_SECRET_KEY: string;
+            JWT_ACCESS_EXPIRES_IN: string;
+            JWT_REFRESH_EXPIRES_IN: string;
+            // TODO SMTP
+            PASSWORD_CODE_TTL: string;
+            DATABASE_URL: string;
+        }
+    }
+}
+
+export { }
