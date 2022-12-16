04f7eb3da7e6240746fe4d373dd317991e9cb641
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-12-16 23:03:42 +0100

    Added NavBar Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
+import { NavBar } from "./components/partials/NavBar"
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <NavBar />
     </div>
   )
 }
