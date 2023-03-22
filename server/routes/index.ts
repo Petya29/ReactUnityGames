f37cc67d3ee8c0388d73922e87bc546c08404c2c
gr00stic <dmitriy.kharabara@gmail.com>
2023-03-22 15:10:35 +0200

    Added game router

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./user.router";
+import gameRouter from "./game.router";
 
 const router = Router();
 
 router.use('/user', userRouter);
+router.use('/game', gameRouter);
 
 export default router;
