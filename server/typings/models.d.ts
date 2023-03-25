5fb3af7351b522505d2da3b74761900b914a5c99
Petya29 <petya29avramenko@gmail.com>
2023-03-25 20:43:42 +0100

    Added typings for new tables

--- server/typings/models.d.ts
+++ server/typings/models.d.ts
@@ -12,7 +12,8 @@ export namespace Models {
         updatedAt: Date;
         lang: Lang;
         refreshToken?: Token[]
-        UserPasswordCode?: UserPasswordCode
+        UserPasswordCode?: UserPasswordCode;
+        UserScores?: UserScores
     }
 
     export interface Token {
@@ -29,6 +30,28 @@ export namespace Models {
         userId: string;
         user?: User;
         passwordCode: string;
+        createdAt: Date;
+        updatedAt: Date;
+    }
+
+    export interface Game {
+        id: string;
+        name: string;
+        description: string;
+        UserScores: UserScores;
+        createdAt: Date;
+        updatedAt: Date;
+    }
+
+    export interface UserScores {
+        id: string;
+        userId: string;
+        user?: User;
+        gameId: string;
+        game?: Game;
+        level: number;
+        score: number;
+        region: string;
         createdAt: Date;
         updatedAt: Date;
     }
